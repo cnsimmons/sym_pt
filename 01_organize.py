df77418e459ccb332ac35a92334f13bfec718d60
cnsimmons <67644546+cnsimmons@users.noreply.github.com>
2026-02-07 10:58:14 -0500

    update params name

--- 01_organize.py
+++ 01_organize.py
@@ -4,7 +4,7 @@
 """
 import os
 import pandas as pd
-from long_pt_params import raw_dir, processed_dir, csv_file, task, skip_subs, get_sessions, get_runs
+from sym_pt_params import raw_dir, processed_dir, csv_file, task, skip_subs, get_sessions, get_runs
 
 
 def setup_subject(sub, sessions):
